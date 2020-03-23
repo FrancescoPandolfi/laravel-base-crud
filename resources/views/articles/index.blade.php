bc2b838986d9b5463c45af8b1d5a5eecc34d1cf8
Francesco Pandolfi <francesco.pan@gmail.com>
2020-03-23 15:23:15 +0100

    index with all articles

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<title>Document</title>
+<link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+
+    @foreach ($articles as $article)
+    <div class="card mb-3">
+        <img class="card-img-top" src="{{ $article->img }}" alt="Card image cap">
+        <div class="card-body">
+            <h2 class="card-title">{{ $article->title }}</h2>
+            <p class="card-text">{{ $article->body }}</p>
+            <p class="card-text"><small class="text-muted">by {{ $article->author }}</small></p>
+        </div>
+    </div>
+    @endforeach
+    </div>
+</div>
+
+</body>
+</html>
